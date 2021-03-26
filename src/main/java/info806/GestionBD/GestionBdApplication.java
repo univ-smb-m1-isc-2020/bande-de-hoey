92daf5eff7b11486d08952223f8195b740726028
Hamze <hamzaalrashed1@hotmail.com>
2021-03-26 15:09:01 +0100

    test

--- src/main/java/info806/GestionBD/GestionBdApplication.java
+++ src/main/java/info806/GestionBD/GestionBdApplication.java
@@ -8,7 +8,6 @@ public class GestionBdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionBdApplication.class, args);
-		System.out.println("Hoey darkness my old friend");
 	}
 
 }
--- src/main/java/info806/GestionBD/dao/FakeUserDataAccessService.java
+++ src/main/java/info806/GestionBD/dao/FakeUserDataAccessService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
 @Repository("fakeDao")
 public class FakeUserDataAccessService implements UserDao{
 
