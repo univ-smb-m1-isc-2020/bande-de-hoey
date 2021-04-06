d6f8ff4c9205b06d5ee0ddc6110639adbc873f05
Hamze <hamzaalrashed1@hotmail.com>
2021-04-06 15:44:17 +0200

    deploy 1

--- src/test/java/info806/GestionBD/GestionBdApplicationTests.java
+++ src/test/java/info806/GestionBD/GestionBdApplicationTests.java
@@ -1,10 +1,11 @@
 package info806.GestionBD;
 
-import org.testng.annotations.Test;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionBdApplicationTests {
+public class GestionBdApplicationTests {
 
 	@Test
 	void contextLoads() {
